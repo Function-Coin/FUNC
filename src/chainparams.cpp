8a1c21fe2e057dbace385b3d46e166b1fe715414
CryptoDev-Project <47718058+CryptoDev-Project@users.noreply.github.com>
2019-08-15 22:18:03 +0100

    Update nPublicZCSpends as some non-zerocoin logic is dependant upon its value

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -150,7 +150,7 @@ public:
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM
 
         // Public coin spend enforcement
-        nPublicZCSpends = 225;
+        nPublicZCSpends = 90000;
 
         // Fake Serial Attack
         nFakeSerialBlockheightEnd = -1;
